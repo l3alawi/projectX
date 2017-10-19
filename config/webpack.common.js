dd8f1336120e9a500569f33c9c44ae27c6f38e9e
l3alawi <alaeddine.beljebbar@gmail.com>
2017-10-19 12:33:03 +0100

    webpack config

---
+++ config/webpack.common.js
@@ -0,0 +1,48 @@
+var helpers = require('./helpers');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+
+module.exports = {
+	entry: './src/index.js',
+
+
+	module: {
+		rules: [
+			{
+				test: /\.js$/, 
+				loader: 'babel-loader', exclude: /node_modules/
+			},
+			{
+				test: /\.jsx$/, 
+				loader: 'babel-loader', exclude: /node_modules/
+			},
+			{
+				test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+				loader: 'file-loader?name=assets/[name].[hash].[ext]' 
+			},
+			{
+				test: /\.css$/,
+				exclude: helpers.root('src'),
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: 'css-loader?sourceMap'
+				})
+			},
+			{
+				test: /\.css$/,
+				include: helpers.root('src'),
+				loader: 'raw-loader'
+			}
+
+		]
+	},
+
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+			filename: 'index.html',
+			inject: 'body'
+		})
+	]
+}
---
+++ src/components/main/main.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+export default class Main extends React.Component {
+	render(){
+		return(
+		<div>
+			<h1>Hello l3alawi lm9awed</h1>
+		</div>
+		)
+	}
+}
